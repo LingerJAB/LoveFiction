785b18948d721bb8be4a3e580c0420405a677bba
Lin <2862125721@qq.com>
2023-09-25 01:25:25 +0800

    (ᵔᵕᵔ˶)

--- NULL/Title2Chaos.java
+++ NULL/Title2Chaos.java
@@ -11,7 +11,7 @@ public class Title2Chaos {
         // 输入待转换文字   TIPS: 请不要输入[.md]
         mkdir("""
                                 
-                23_问题
+                25_秋
                                 
                 """.trim(), Title2Chaos::encode);
     }
